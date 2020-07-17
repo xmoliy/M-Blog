ce1ed43423134fd8440b67fb27bff2124af2abe9
Mo1iy <38353840+xmoliy@users.noreply.github.com>
2020-07-17 11:12:35 +0800

    Update settings.py

--- realBlog/settings.py
+++ realBlog/settings.py
@@ -146,10 +146,10 @@ TINYMCE_DEFAULT_CONFIG = {
 CACHES={
     'default':{
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://120.79.223.66:6379/1",
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "PASSWORD": "8227099"
+            "PASSWORD": "123"
         }
     }
 }
